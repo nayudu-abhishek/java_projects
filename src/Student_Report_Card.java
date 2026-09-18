06ffc2353982988aacc73ff2c6a9d1be0011bded
Nayudu Abhishek <naiduabhishek006@gmail.com>
2026-09-18 17:28:57 +0530

    Added scanner class

--- src/Student_Report_Card.java
+++ src/Student_Report_Card.java
@@ -1,14 +1,21 @@
+import com.sun.source.doctree.EscapeTree;
 import org.w3c.dom.ls.LSOutput;
 
 import java.lang.classfile.instruction.StackInstruction;
+import java.util.Scanner;
 
 //Project: Student Report Card Calculator
 //Store a student's name and marks in 5 subjects,
 // then calculate total, average, and grade.
 public  class Student_Report_Card {
     public static void main(String[] args) {
-        String name = "john";
-        int maths = 70, science = 85, python = 96;
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int maths = sc.nextInt(), science = sc.nextInt(), python = sc.nextInt();
+        System.out.println("Enter Student name"+name);
+        System.out.println("Enter ur maths marks:"+maths);
+        System.out.println("Entere ur science marks"+science);
+        System.out.println("Enter ur python marks:"+python);
         int total = (maths + science + python);
         double average = (maths + science + python) / 3;
         char grade = average >= 90 ? 'A' : average >= 70 ? 'B' : 'C';
