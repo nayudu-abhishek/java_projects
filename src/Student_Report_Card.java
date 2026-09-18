dec7cc7463fc8886757e9365ba99d10343ef1f8a
Nayudu Abhishek <naiduabhishek006@gmail.com>
2026-09-18 12:02:30 +0530

    The studentReportCard project was added on the git

---
+++ src/Student_Report_Card.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.lang.classfile.instruction.StackInstruction;
+
+//Project: Student Report Card Calculator
+//Store a student's name and marks in 5 subjects,
+// then calculate total, average, and grade.
+public  class Student_Report_Card {
+    public static void main(String[] args) {
+        String name = "john";
+        int maths = 70, science = 85, python = 96;
+        int total = (maths + science + python);
+        double average = (maths + science + python) / 3;
+        char grade = average >= 90 ? 'A' : average >= 70 ? 'B' : 'C';
+        System.out.println("Name:"+name +"\ntotal:"+total+"\naverage"+average+"\nGrade"+grade);
+    }
+}
