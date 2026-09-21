8e4445c7073878030d9d98975ea068569e35fd97
Nayudu Abhishek <naiduabhishek006@gmail.com>
2026-09-21 19:55:28 +0530

    BankAccountSystem added successfully in the concept of the methods

---
+++ src/Bank_Account_System.java
@@ -0,0 +1,33 @@
+public class Bank_Account_System {
+    String name;
+    String AccountName;
+    double balance;
+    Bank_Account_System(String name,String AccountName,double balance){
+        this.name = name;
+        this.AccountName = AccountName;
+        this.balance = balance;
+    }
+    public  void getDetails(){
+        System.out.println("Name:"+""+name+"\nAccountName:"+""+AccountName+""+"\nbalance:"+balance);
+    }
+   public void deposit(double amount){
+       balance += amount;
+       System.out.println("Total balance of your account:"+balance);
+   }
+   public boolean withDrawel(double amount){
+       if(amount > balance){
+           System.out.println("Insufficient amount");
+           return false;
+       }
+       balance -= amount;
+       System.out.println("Total balance in your account is:"+balance);
+       return  true;
+   }
+
+    public static void main(String[] args) {
+        Bank_Account_System ba = new Bank_Account_System("John","ap123",1000);
+        ba.getDetails();
+        ba.deposit(100);
+        System.out.println(ba.withDrawel(600));
+    }
+}
